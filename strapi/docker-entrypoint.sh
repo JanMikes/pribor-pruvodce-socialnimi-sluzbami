2b3686a2d6788973789b64b5099a0cb00b00c4d9
Jan Mikeš <j.mikes@me.com>
2025-11-29 19:56:16 +0100

    Fixes for local development

--- strapi/docker-entrypoint.sh
+++ strapi/docker-entrypoint.sh
@@ -4,7 +4,17 @@ set -ea
 # Install npm dependencies if node_modules is missing
 if [ ! -d "node_modules" ]; then
   echo "Installing npm dependencies..."
-  npm ci
+  if [ -f "package-lock.json" ]; then
+    npm ci
+  else
+    npm install
+  fi
+fi
+
+# Build strapi if dist is missing or outdated
+if [ ! -d "dist" ] || [ "src" -nt "dist" ]; then
+  echo "Building Strapi..."
+  npm run build
 fi
 
 exec "$@"
