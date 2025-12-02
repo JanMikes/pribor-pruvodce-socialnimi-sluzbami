398e560b23072d05aa37f18a6c13497024ad6201
Jan Mikeš <j.mikes@me.com>
2025-12-02 22:35:26 +0100

    Styling

--- frontend/src/app/krizove-linky/page.tsx
+++ frontend/src/app/krizove-linky/page.tsx
@@ -60,7 +60,7 @@ export default async function CrisisLinesPage() {
                       <div className="flex-shrink-0 sm:self-stretch">
                         <a
                           href={`tel:${line.phone}`}
-                          className="btn-secondary text-base w-full sm:h-full"
+                          className="btn-secondary text-base w-full sm:h-full min-w-[225px]"
                         >
                           <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
@@ -111,6 +111,9 @@ export default async function CrisisLinesPage() {
                               {line.email}
                             </a>
                           )}
+                          {line.email && line.website && (
+                            <span className="text-stone-300">|</span>
+                          )}
                           {line.website && (
                             <a
                               href={line.website}
@@ -121,7 +124,7 @@ export default async function CrisisLinesPage() {
                               <svg className="w-4 h-4 mr-1.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                               </svg>
-                              Web
+                              Navštívit web
                             </a>
                           )}
                         </div>
